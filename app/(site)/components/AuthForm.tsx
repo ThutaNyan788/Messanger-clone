2440be795aebad54bbc2a04af95690c108c227a2
thutanyan <thutathuta319@gmail.com>
2023-11-24 16:50:42 +0630

    added Login setup

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -11,6 +11,8 @@ import Button from '@/app/(site)/components/Button';
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs'
 import axios from 'axios';
+import {toast} from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -45,11 +47,29 @@ const AuthForm = () => {
 
         if (variant === 'REGISTER') {
             //TODO:Axios Register
-            axios.post('/api/register',data);
+            axios.post('/api/register',data)
+            .catch(()=>toast.error("Something went wrong"))
+            .finally(()=>setIsLoading(false));
+
+            
         }
 
         if (variant === 'LOGIN') {
             //TODO:NextAuth Signin
+            signIn('credentials',{
+                ...data,
+                redirect:false
+            })
+            .then((callback)=>{
+                if(callback?.error){
+                    toast.error("Invalid Credentials")
+                }
+
+                if(callback?.ok && !callback?.error){
+                    toast.success("Logged In");
+                }
+            })
+            .finally(()=>setIsLoading(false));
         }
     }
 
@@ -57,6 +77,18 @@ const AuthForm = () => {
         setIsLoading(true);
 
         //TODO:NextAuth Social Login
+        signIn(action,{redirect:false})
+        .then((callback)=>{
+            if(callback?.error){
+                toast.error("Invalid Credentials");
+            }
+
+            if(callback?.ok && !callback?.error){
+                toast.success("Logged In");
+            }
+        })
+        .finally(()=>setIsLoading(false));
+
     }
 
     return (
