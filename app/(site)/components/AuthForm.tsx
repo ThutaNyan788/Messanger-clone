29469fa859f0c13a1285f48d3a9cc061723c5f8f
thutanyan <thutathuta319@gmail.com>
2023-11-25 09:55:53 +0630

    added middleware

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import {
     FieldValues,
     useForm,
@@ -13,12 +13,22 @@ import { BsGithub, BsGoogle } from 'react-icons/bs'
 import axios from 'axios';
 import {toast} from 'react-hot-toast';
 import { signIn } from 'next-auth/react';
+import {useSession} from 'next-auth/react'
+import { useRouter } from 'next/navigation';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(()=>{
+        if(session?.status === 'authenticated'){
+            router.push("/users")
+        }
+    },[session?.status,router])
 
     const toggleVariant = useCallback(() => {
         if (variant === 'LOGIN') {
@@ -48,6 +58,7 @@ const AuthForm = () => {
         if (variant === 'REGISTER') {
             //TODO:Axios Register
             axios.post('/api/register',data)
+            .then(()=>signIn("credentials",data))
             .catch(()=>toast.error("Something went wrong"))
             .finally(()=>setIsLoading(false));
 
@@ -67,6 +78,7 @@ const AuthForm = () => {
 
                 if(callback?.ok && !callback?.error){
                     toast.success("Logged In");
+                    router.push("/users")
                 }
             })
             .finally(()=>setIsLoading(false));
